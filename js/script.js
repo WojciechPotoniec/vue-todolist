64fae9dfec69a5149a631a3f8b2ac2bc09f0dc71
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-02 17:09:27 +0200

    added some method on methods in js. toggleDone, deleteItem and addItem function

--- js/script.js
+++ js/script.js
@@ -2,18 +2,51 @@ import { todo } from "./data.js";
 
 // console.log(todo);
 
-const {createApp} = Vue;
+const { createApp } = Vue;
 
 createApp({
-    data() {
-        return {
-            todo,
-        };
-    },
-    methods:{
+  data() {
+    return {
+      todo,
+      itemText: "",
+      done: "",
+    };
+  },
+  methods: {
+    toggleDone(id) {
+        const item = this.todo.find((el) => {
+          return el.id === id;
+        });
+        // console.log(item);
+        if (item) {
+          item.done = !item.done;
+        }
+        // this.todo[i].done = !this.todo[i].done
+      },
+    deleteItem(id) {
+        const i = this.todo.findIndex((el) => el.id === id);
+        if (i !== -1) {
+          this.todo.splice(i, 1);
+        }
+      },
+      addItem() {
+        const newObj = {
+          id: null,
+          text: this.itemText,
+          done: false,
+        }
+        let nextId = 0;
+        this.todo.forEach((el)=>{
+          if(nextId < el.id){
+              nextId = el.id;
+          }
+        });
+        newObj.id = nextId +1;
+        this.todo.push(newObj);
+        this.itemText = '';
+      },
+  },
+  mounted() {
 
-    },
-    mounted(){
-
-    }
-}).mount('#app');
+  },
+}).mount("#app");
