77962957f19ef743917112e4589048a080af6130
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-02 14:52:20 +0200

    destructured createApp and associated to Vue; after initialised createApp associated from data.js a variable in return of data to take the array of object

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 import { todo } from "./data.js";
 
 // console.log(todo);
+
+const {createApp} = Vue;
+
+createApp({
+    data() {
+        return {
+            todo,
+        };
+    },
+    mounted(){
+        
+    }
+}).mount('app');
