1640f48eba7cb81f09116e878061c752b515f1ec
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-02 17:13:06 +0200

    added computed property to create an other array of object

--- js/script.js
+++ js/script.js
@@ -14,39 +14,52 @@ createApp({
   },
   methods: {
     toggleDone(id) {
-        const item = this.todo.find((el) => {
-          return el.id === id;
-        });
-        // console.log(item);
-        if (item) {
-          item.done = !item.done;
-        }
-        // this.todo[i].done = !this.todo[i].done
-      },
+      const item = this.todo.find((el) => {
+        return el.id === id;
+      });
+      // console.log(item);
+      if (item) {
+        item.done = !item.done;
+      }
+      // this.todo[i].done = !this.todo[i].done
+    },
     deleteItem(id) {
-        const i = this.todo.findIndex((el) => el.id === id);
-        if (i !== -1) {
-          this.todo.splice(i, 1);
+      const i = this.todo.findIndex((el) => el.id === id);
+      if (i !== -1) {
+        this.todo.splice(i, 1);
+      }
+    },
+    addItem() {
+      const newObj = {
+        id: null,
+        text: this.itemText,
+        done: false,
+      };
+      let nextId = 0;
+      this.todo.forEach((el) => {
+        if (nextId < el.id) {
+          nextId = el.id;
         }
-      },
-      addItem() {
-        const newObj = {
-          id: null,
-          text: this.itemText,
-          done: false,
+      });
+      newObj.id = nextId + 1;
+      this.todo.push(newObj);
+      this.itemText = "";
+    },
+  },
+  computed: {
+    filteredTodo() {
+      return this.todo.filter((el) => {
+        if (this.done === "") {
+          return true;
         }
-        let nextId = 0;
-        this.todo.forEach((el)=>{
-          if(nextId < el.id){
-              nextId = el.id;
-          }
-        });
-        newObj.id = nextId +1;
-        this.todo.push(newObj);
-        this.itemText = '';
-      },
+        if (this.done === "false") {
+          return el.done === false;
+        }
+        if (this.done === "true") {
+          return el.done === true;
+        }
+      });
+    },
   },
-  mounted() {
-
-  },
+  mounted() {},
 }).mount("#app");
