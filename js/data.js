24e61f71dd372c357bf8e779f4751bc0c3e96ff9
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-04-02 14:41:43 +0200

    initialaised list in data.js and exported

---
+++ js/data.js
@@ -0,0 +1,17 @@
+export const todo = [
+    {
+        id: 1,
+        text: 'Documenti',
+        done: false
+    },
+    {
+        id: 2,
+        text: 'Valigia',
+        done: false
+    },
+    {
+        id: 3,
+        text: 'Contanti',
+        done: false
+    }
+]
